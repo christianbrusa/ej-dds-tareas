24107923de0cfbaa92d0fdf3b58512d410e6f490
christianbrusa <christianbrusa@hotmail.com>
2023-01-12 01:31:54 -0300

    Agrego clase de tarea Simple

--- ej-dds-tareas.ts
+++ ej-dds-tareas.ts
@@ -9,3 +9,20 @@ interface Tarea {
   obtenerHorasNecesarias(): Number;
   obtenerCosto(): Number;
 }
+
+class Simple implements Tarea {
+  constructor(cantidadEstimadaHoras: Number, responsable: Object, empleadosAsignados: Array<Object>, costoDeInfraestructura: Number) {
+    this.cantidadEstimadaHoras = cantidadEstimadaHoras;
+    this.responsable = responsable;
+    this.empleadosAsignados = empleadosAsignados;
+    this.costoDeInfraestructura = costoDeInfraestructura;
+  }
+  
+  obtenerHorasNecesarias() {
+
+  }
+  
+  obtenerCosto() {
+    
+  }
+}
